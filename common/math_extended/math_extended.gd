42bee320f012b4b0ccc62d059bf09ac4138538a1
Veeron Ten <veeronten@gmail.com>
2025-08-21 00:15:27 +0300

    add distance for light gun

---
+++ common/math_extended/math_extended.gd
@@ -0,0 +1,4 @@
+class_name MathExtended
+
+static func is_in_range(value: Variant, min: Variant, max: Variant) -> bool:
+	return value >= min and value <= max
--- entity/gun/light/light_gun.gd
+++ entity/gun/light/light_gun.gd
@@ -1,5 +1,15 @@
+@tool
 class_name LightGun
 extends Node2D
+
+@export var _max_distance: float:
+	set(value):
+		assert(value >= 0, "distance cant be less than 0")
+		_max_distance = max(0, value)
+@export var _distance_to_shoot_above_ground: float:
+	set(value):
+		assert(MathExtended.is_in_range(value, 0.0, _max_distance), "the distance is restricted by max_distance and cant be < 0")
+		_distance_to_shoot_above_ground = clamp(value, 0, _max_distance)
 
 @onready var _pivot: Node2D = %Pivot
 @onready var _sprite_2d: Sprite2D = %Sprite2D
@@ -16,4 +26,6 @@ var pointer_position: Vector2 = Vector2.ZERO:
 
 func shoot() -> void:
 	var distance: float = pointer_position.distance_to(_damaging_ray_component.global_position)
+	if distance > _distance_to_shoot_above_ground:
+		distance = _max_distance
 	_damaging_ray_component.shoot(distance)
