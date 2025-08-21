ed4ad2720fe81e197fd12d0e12e9018fdb34a686
Veeron Ten <veeronten@gmail.com>
2025-08-21 13:28:18 +0300

    apply collisions recolorer

--- component/damage/damageable_component/damageable_component.gd
+++ component/damage/damageable_component/damageable_component.gd
@@ -1,4 +1,3 @@
-@tool
 class_name DamageableComponent
 extends Area2D
 
@@ -6,17 +5,8 @@ extends Area2D
 @export var _health: HealthComponent
 #todo как запретить создание ноды и оставить только сцену?
 
-func _ready() -> void:
-	if Engine.is_editor_hint: _recolor_debug_areas()
-
 func take_damage(damage: Damage) -> void:
 	var resulting_damage: Damage = damage
 	if _damage_filter != null:
 		resulting_damage = _damage_filter.filter_damage(resulting_damage)
 	_health.take_damage(resulting_damage)
-
-func _recolor_debug_areas() -> void:
-	for child: Node in get_children():
-		if child is CollisionShape2D:
-			var collision: CollisionShape2D = child
-			collision.debug_color = Color.hex(0x519d536b)
