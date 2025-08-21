3cc50b4b6a7fbb508731a6718a9e4fa3d1a61019
Veeron Ten <veeronten@gmail.com>
2025-08-21 13:27:36 +0300

    add collisions recolorer for future addon

---
+++ addons/personal/collisions_recolorer/collisions_recolorer.gd
@@ -0,0 +1,16 @@
+@tool
+class_name CollisionsRecolorer
+extends Node
+
+@export var collisions_color: Color
+@export var target: Node
+
+#todo унести в плагин
+func _ready() -> void:
+	if Engine.is_editor_hint: _recolor_debug_areas()
+
+func _recolor_debug_areas() -> void:
+	for child: Node in target.get_children():
+		if child is CollisionShape2D:
+			var collision: CollisionShape2D = child
+			collision.debug_color = collisions_color
