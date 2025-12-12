5362d37235491246d73fbe9c9a0d5e89d7ebb970
Veeron Ten <veeronten@gmail.com>
2025-12-12 21:17:25 +0100

    make acid do continuous damage, to dummies too

--- component/damage/damage.gd
+++ component/damage/damage.gd
@@ -6,3 +6,5 @@ extends Resource
 	set(new_value):
 		assert(new_value >= 0.0, "damage cant be < 0")
 		value = new_value
+		
+@export var is_continuous: bool
--- component/damage/damaging_area_component/damaging_area_component.gd
+++ component/damage/damaging_area_component/damaging_area_component.gd
@@ -12,8 +12,22 @@ func _ready() -> void:
 	assert(_damage != null, "damage must be set")
 	assert(_collision_filter != null, "coolision filter must be set")
 
+func _physics_process(delta: float) -> void:
+	if not _damage.is_continuous: return
+	var areas: Array[Area2D] = get_overlapping_areas()
+	for area: Area2D in areas:
+		if area is DamageableComponent:
+			var damageable_area: DamageableComponent = area
+			if (_collision_filter.is_compatible_with(damageable_area.collision_filter)):
+				var continuous_damage: Damage = _damage.duplicate()
+				continuous_damage.value = _damage.value * delta
+				damageable_area.take_damage(continuous_damage)
+			
 func _on_area_entered(area: Area2D) -> void:
+	if _damage.is_continuous: return
 	if area is DamageableComponent:
 		var damageable_area: DamageableComponent = area
 		if (_collision_filter.is_compatible_with(damageable_area.collision_filter)):
 			damageable_area.take_damage(_damage)
+			
+			
