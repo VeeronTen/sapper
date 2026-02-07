81c42235b05f4e4c62e338cc2b1b0c747dbfae64
Veeron Ten <veeronten@gmail.com>
2026-02-07 16:34:18 +0100

    change interacting object logic

--- component/interaction/interacting_component/interacting_component.gd
+++ component/interaction/interacting_component/interacting_component.gd
@@ -5,6 +5,13 @@ extends Area2D
 
 signal can_interact_changed(can_interact: bool)
 
+var _can_interact: bool:
+	set(value):
+		if _can_interact != value:
+			can_interact_changed.emit(value)
+		_can_interact = value
+var _targets: Array[InteractableComponent] = []
+
 var _interactable_target: InteractableComponent:
 	set(value):
 		if value == _interactable_target:
@@ -14,33 +21,38 @@ var _interactable_target: InteractableComponent:
 		_interactable_target = value
 		if is_instance_valid(value):
 			value.set_is_interactable(true)
-		
-var _interactables_nearby: int = 0:
-	set(value):
-		if value == 0 and _interactables_nearby != 0:
-			can_interact_changed.emit(false)
-		if value != 0 and _interactables_nearby == 0:
-			can_interact_changed.emit(true)
-		_interactables_nearby = value
 
+func  _physics_process(_delta: float) -> void:
+	_process_targets()
+	
 func _on_area_entered(area: Area2D) -> void:
-	if area is not InteractableComponent:
+	if not _filter(area):
 		return
-	if not _can_interact_with_tags.has((area as InteractableComponent).interaction_tag):
-		return
-	_interactable_target = area
-	_interactables_nearby += 1
+	_targets.append(area)
 
 func _on_area_exited(area: Area2D) -> void:
-	if area is not InteractableComponent:
+	if not _filter(area):
 		return
-	if not _can_interact_with_tags.has((area as InteractableComponent).interaction_tag):
-		return
-	if area == _interactable_target:
-		_interactable_target = null
-	_interactables_nearby -= 1
+	_targets.erase(area)
 
 func interact() -> String:
 	if is_instance_valid(_interactable_target):
 		return _interactable_target.interact()
 	return "NO INTERACTION"
+	
+func _filter(area: Area2D) -> bool:
+	if area is not InteractableComponent:
+		return false
+	if not _can_interact_with_tags.has((area as InteractableComponent).interaction_tag):
+		return false
+	return true
+
+func _process_targets() -> void:
+	_can_interact = not _targets.is_empty()
+	_interactable_target = _closest_target()
+
+func _closest_target() -> InteractableComponent:
+	return _targets.reduce(
+		func(prev: InteractableComponent, curr: InteractableComponent) -> InteractableComponent: 
+			return curr if global_position.distance_squared_to(curr.global_position) < global_position.distance_squared_to(prev.global_position) else prev
+	)
