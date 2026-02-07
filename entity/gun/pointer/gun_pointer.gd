d480e4d2ced6c3c78c0bf4c9e0e4136bb03c590b
Veeron Ten <veeronten@gmail.com>
2026-02-07 23:45:34 +0100

    pointer marker

--- entity/gun/pointer/gun_pointer.gd
+++ entity/gun/pointer/gun_pointer.gd
@@ -1,6 +1,18 @@
+@tool
 class_name GunPointer
 extends RayCast2D
 
+@export var marker: Texture2D: 
+	set(value):
+		marker = value
+		if sprite_2d:
+			sprite_2d.texture = marker
+
+@onready var sprite_2d: Sprite2D = %Sprite2D
+
+func _ready() -> void:
+	marker = marker
+	
 func get_damageable_component_at_pointer() -> DamageableComponent:
 	force_raycast_update()
 	while is_colliding():
