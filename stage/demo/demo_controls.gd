73431f2fa3566bab8e293659e61aa218903251db
Veeron Ten <veeronten@gmail.com>
2026-02-12 19:45:36 +0100

    new custom input events

--- stage/demo/demo_controls.gd
+++ stage/demo/demo_controls.gd
@@ -3,15 +3,23 @@ extends Node2D
 signal move_direction_changed(direction: Vector2)
 signal is_moving_changed(is_moving: bool)
 signal pointer_position_changed(pointer: Vector2)
+signal pointer_pressed(is_pressed: bool)
 signal pointer_click(pointer: Vector2)
 signal roll_pressed
 signal interact_pressed
 signal drop_pressed
+var _move_direction: Vector2 = Vector2.ZERO:
+	set(value):
+		if _move_direction != value:
+			_move_direction = value
+			move_direction_changed.emit(_move_direction)
+			_is_moving = _move_direction != Vector2.ZERO
 
-var _prev_move_direction: Vector2 = Vector2.ZERO
-var _prev_is_moving: bool = false
-
-signal pointer_pressed(is_pressed: bool)
+var _is_moving: bool = false:
+	set(value):
+		if _is_moving != value:
+			_is_moving = value
+			is_moving_changed.emit(_is_moving)
 
 func _process(_delta: float) -> void:
 	pointer_position_changed.emit(get_global_mouse_position())
@@ -21,32 +29,25 @@ func _process(_delta: float) -> void:
 		pointer_pressed.emit(false)
 	
 func _physics_process(_delta: float) -> void:
-	var move_direction: Vector2 = Input.get_vector('ui_left', 'ui_right', 'ui_up', 'ui_down')
-	if move_direction != _prev_move_direction:
-		move_direction_changed.emit(move_direction)
-		_prev_move_direction = move_direction
-		var is_moving: bool = move_direction != Vector2.ZERO
-		if is_moving != _prev_is_moving:
-			is_moving_changed.emit(is_moving)
-			_prev_is_moving = is_moving
-			
+	_move_direction = Input.get_vector('move_left', 'move_right', 'move_up', 'move_down')
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		var mouse_event: InputEventMouseButton = event
 		if mouse_event.pressed:
-			_handle_mouse(mouse_event)
+			_handle_mouse_input(mouse_event)
 	elif event is InputEventKey:
 		var key_event: InputEventKey = event
 		if key_event.pressed and not key_event.echo:
-			_handle_key(key_event)
+			_handle_key_input(key_event)
 
-func _handle_mouse(event: InputEventMouseButton) -> void:
+func _handle_mouse_input(event: InputEventMouseButton) -> void:
 	match event.button_index:
 		MOUSE_BUTTON_LEFT: 
-			pointer_click.emit(get_global_mouse_position())		
-	
-func _handle_key(event: InputEventKey) -> void:
-	if event.is_action("ui_accept"):
+			pointer_click.emit(get_global_mouse_position())
+			
+func _handle_key_input(event: InputEventKey) -> void:
+	if event.is_action("roll"):
 		roll_pressed.emit()
 	elif event.is_action("interact"):
 		interact_pressed.emit()
